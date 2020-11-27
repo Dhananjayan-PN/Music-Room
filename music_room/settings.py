e16274f887a6be186d91762ae9d115dd97222256
Dhananjayan P N <pndjay@gmail.com>
2020-11-27 18:34:51 +0000

    sslserver and other updates

--- music_room/settings.py
+++ music_room/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'rest_framework',
     'frontend.apps.FrontendConfig',
+    'sslserver',
 ]
 
 MIDDLEWARE = [
@@ -117,5 +119,4 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-STATIC_URL = '/static/'
+STATIC_URL = '/frontend/static/'
