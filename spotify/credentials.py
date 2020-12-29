3b3414eb92cd7bf71217112de2524d75c1287b37
Dhananjayan P N <pndjay@gmail.com>
2020-12-29 11:26:06 +0530

    update redirect

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,3 @@
 CLIENT_ID = "713ab540b67a4e2eb1ec820fdeb1a84a"
 CLIENT_SECRET = "4cda577dc54c4342a2be4970bed55ca7"
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
+REDIRECT_URI = "https://musicroom.ml/spotify/redirect"
